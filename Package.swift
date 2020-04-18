23ebf63959fec3930e527dcf72bfd5e7942bedd3
Rob Mecham <stemmetje@gmail.com>
2020-04-18 14:36:12 -0600

    Switched to a forked version of Ink.

--- Package.swift
+++ Package.swift
@@ -9,37 +9,37 @@
 import PackageDescription
 
 let package = Package(
-    name: "Publish",
-    products: [
-        .library(name: "Publish", targets: ["Publish"]),
-        .executable(name: "publish-cli", targets: ["PublishCLI"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/johnsundell/ink.git", from: "0.2.0"),
-        .package(url: "https://github.com/johnsundell/plot.git", from: "0.4.0"),
-        .package(url: "https://github.com/johnsundell/files.git", from: "4.0.0"),
-        .package(url: "https://github.com/johnsundell/codextended.git", from: "0.1.0"),
-        .package(url: "https://github.com/johnsundell/shellout.git", from: "2.3.0"),
-        .package(url: "https://github.com/johnsundell/sweep.git", from: "0.4.0")
-    ],
-    targets: [
-        .target(
-            name: "Publish",
-            dependencies: [
-                "Ink", "Plot", "Files", "Codextended", "ShellOut", "Sweep"
-            ]
-        ),
-        .target(
-            name: "PublishCLI",
-            dependencies: ["PublishCLICore"]
-        ),
-        .target(
-            name: "PublishCLICore",
-            dependencies: ["Publish"]
-        ),
-        .testTarget(
-            name: "PublishTests",
-            dependencies: ["Publish", "PublishCLICore"]
-        )
-    ]
+  name: "Publish",
+  products: [
+    .library(name: "Publish", targets: ["Publish"]),
+    .executable(name: "publish-cli", targets: ["PublishCLI"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/rmecham/ink.git", .branch("master")),
+    .package(url: "https://github.com/johnsundell/plot.git", from: "0.4.0"),
+    .package(url: "https://github.com/johnsundell/files.git", from: "4.0.0"),
+    .package(url: "https://github.com/johnsundell/codextended.git", from: "0.1.0"),
+    .package(url: "https://github.com/johnsundell/shellout.git", from: "2.3.0"),
+    .package(url: "https://github.com/johnsundell/sweep.git", from: "0.4.0"),
+  ],
+  targets: [
+    .target(
+      name: "Publish",
+      dependencies: [
+        "Ink", "Plot", "Files", "Codextended", "ShellOut", "Sweep"
+      ]
+    ),
+    .target(
+      name: "PublishCLI",
+      dependencies: ["PublishCLICore"]
+    ),
+    .target(
+      name: "PublishCLICore",
+      dependencies: ["Publish"]
+    ),
+    .testTarget(
+      name: "PublishTests",
+      dependencies: ["Publish", "PublishCLICore"]
+    ),
+  ]
 )
